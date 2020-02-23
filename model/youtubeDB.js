1532b980108f82d2c0f5d989f490c38148037435
zebanavgurukul <zeba18@navgurukul.org>
2020-02-23 04:32:24 +0200

    commit

--- model/youtubeDB.js
+++ model/youtubeDB.js
@@ -6,8 +6,23 @@ let post_data = (data) => {
 };
 
 // 2
+let putdata = (id,updata) => {
+    return knex('userTables').update(updata).where('id',id)
+};
+
+// 3
 let postdata = (Data) => {
     return knex('Feature_Tables').insert(Data)
 };
 
-module.exports = {post_data,postdata}
+// 4
+let getdata = () => {
+    return knex('userTables').select('*')
+};
+
+// 5
+let put_data = (updata,search) => { 
+    return knex.select('*').from('userTables').update(updata).where('description','like',  '%' +search+ '%')
+};
+
+module.exports = {post_data,putdata,postdata,getdata,put_data}
--- routes/youtube.js
+++ routes/youtube.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const youtube = express.Router();
 const youtubeDB   = require("../model/youtubeDB")
+var getYouTubeID = require('get-youtube-id');
 
 // 1
 youtube.post('/post',(req,res) => {
@@ -18,6 +19,22 @@ youtube.post('/post',(req,res) => {
 });
 
 // 2
+youtube.put('/putData/:id',(req,res) => {
+    var id = req.params.id
+    var updata = {
+        title : req.body.title,
+        description : req.body.description,
+        video : req.body.video
+    }
+    youtubeDB.putdata(id,updata) 
+    .then(() => {
+        res.send('update')
+    }).catch((err) => {
+        res.send(err)
+    })
+});
+
+// 3
 youtube.post('/postData',(req,res) => {
     var data = {
         Feature : req.body.Feature,
@@ -28,6 +45,34 @@ youtube.post('/postData',(req,res) => {
     youtubeDB.postdata(data)
     .then(() => {
         res.send("insert")
+    }).catch((err) => {
+        res.send(err)
+    })
+});
+
+// 4
+youtube.get('/get',(req,res) => {
+    youtubeDB.getdata()
+    .then((Response) => {
+    for (var i = 0; i < Response.length; i++){
+        var video = Response[i]['video']
+        var video_id = getYouTubeID(video);
+    }
+    }).catch((err) => {
+        res.send(err)
+    })
+});
+
+youtube.put('/put/:search',(req,res) => {
+    var search = req.params.search
+    var updata = {
+        title : req.body.title,
+        description : req.body.description,
+        video : req.body.video
+    }
+    youtubeDB.put_data(updata,search)
+    .then(() => {
+        res.send('update')
     }).catch((err) => {
         res.send(err)
     })
